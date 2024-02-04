2c07fd640a378c08ecee1037160802d3d69bc9e3
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-02-04 03:39:25 -0500

    add bootstrap and integrate bootstrap with webpack

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './scss/style.scss';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World!!!</h1>
+        <div class="container py-4 px-3 mx-auto">
+            <h1>Hello, Bootstrap and Webpack!</h1>
+            <button class="btn btn-primary">Primary button</button>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/index.js', // Entry point of your application
@@ -17,6 +20,34 @@ module.exports = {
                         presets: ['@babel/preset-env', '@babel/preset-react'] // Use presets for transpiling ES6/React code
                     }
                 }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: miniCssExtractPlugin.loader
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
             }
         ]
     },
@@ -25,5 +56,9 @@ module.exports = {
             directory: path.join(__dirname, 'dist'),
         },
         historyApiFallback: true,
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new miniCssExtractPlugin()
+    ]
 };
