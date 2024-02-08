bb89ec3c7bffd9b8a342e7a2a29e881c851746db
mongo <sihamongkul123@gmail.com>
2024-02-09 01:48:54 +1100

    added the nav bar(Unfinished)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Nav from './components/Nav'
 
 const App = () => {
     return (
-        <div class="container py-4 px-3 mx-auto">
-            <h1>Hello, Bootstrap and Webpack!</h1>
-            <button class="btn btn-primary">Primary button</button>
+        <div>
+            <Nav />
+
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,37 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
 import '../scss/style.scss';
+import "../scss/_variables.scss";
 
 export default function Nav() {
     return (
-        <>
-        </>
+        <nav className ="navbar navbar-expand-xxl bg-primary ">
+        <div className="container-fluid d-flex flex-column align-items-start">
+          <a className="navbar-brand" href="#">Therapy Talks</a>
+
+        
+            <div className="collapse navbar-collapse " >
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Our Mission</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Programs</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Blogs</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Communities</a>
+                </li>
+            
+                </ul>
+            </div>
+        </div>
+        
+      </nav>
+      
+      
     );
 }
 
