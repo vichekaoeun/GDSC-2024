967f89bc05642360c453cb40e57cb034eae76233
WumbleCat <komethtauch04@gmail.com>
2024-02-09 00:05:28 +0000

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import "./scss/_variables.scss"
 import './scss/style.scss';
 import Nav from './components/Nav'
 
 const App = () => {
     return (
-        <div class="container py-4 px-3 mx-auto">
+        <div className="container py-4 px-3 mx-auto bg-primary">
             <h1>Hello, Bootstrap and Webpack!</h1>
-            <button class="btn btn-primary">Primary button</button>
+            <button className="btn btn-primary">Primary button</button>
         </div>
     )
 }
