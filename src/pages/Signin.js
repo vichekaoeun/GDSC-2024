5f6ef55fa5186304ded6e9d16313e142db1611b5
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-02-15 03:23:36 -0500

    add .env file

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCCMbqv6Zz8Op91vz9PZ_A24j7lFUZdxfA",
-    authDomain: "therapytalk-395da.firebaseapp.com",
-    projectId: "therapytalk-395da",
-    storageBucket: "therapytalk-395da.appspot.com",
-    messagingSenderId: "593810777330",
-    appId: "1:593810777330:web:2d48a3aeb80bb77abadc6b",
-    measurementId: "G-WBVM3FF8FG"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig); // Initialize Firebase app
