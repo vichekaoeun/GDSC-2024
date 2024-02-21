5b2b8d02f2ae02ab81930a746c73120768109607
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-02-21 07:27:56 -0500

    change boilerplate of blog page

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -3,6 +3,7 @@ import Nav from '../components/Nav';
 import '../scss/style.scss';
 import '../scss/_variables.scss';
 import './blog.css';
+import SingleProject from "../components/single-project";
 
 export default function Blog() {
     return (
@@ -11,40 +12,30 @@ export default function Blog() {
                 <Nav />
             </div>
             <div className="post-bg rounded m-5 min-vh-100" style={{ display: 'flex', flexDirection: 'column' }}>
-                <div className="rounded m-5 bg-white">
-                    <div className="m-3">
-                        <div>
-                            <div className="row justify-content-center">
-                                Title
-                            </div>
-                            <div>
-                                Name | Date
-                            </div>
-                        </div>
-                        <div>
-                            Tags
-                        </div>
-                        <div className="row justify-content-center">
-                            Content
-                        </div>
+                <div className="card mb-4 m-5">
+                    {/* Post Details */}
+                    <div className="card-header">
+                        <small className="text-muted">Author: John Doe | Posted on: October 5, 2023</small>
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">Post Title</h5>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores saepe adipisci totam cum eum tempore veritatis odio, nulla est vero autem iusto ducimus, labore illo neque! Voluptatum non voluptates optio enim aliquid obcaecati aut similique. Voluptates consequuntur eius pariatur perspiciatis non dolores nihil quos, quod delectus ea laboriosam voluptatibus quasi. Necessitatibus illo, ipsam omnis placeat aperiam laboriosam autem rem, deleniti soluta tempore, accusantium tenetur?.</p>
+                    </div>
+                    <div className="p-4">
+                        <u>Comments: 5</u>
                     </div>
                 </div>
-                <div className="rounded m-5 bg-white">
-                    <div className="m-3">
-                        <div>
-                            <div className="row justify-content-center">
-                                Title
-                            </div>
-                            <div>
-                                Name | Date
-                            </div>
-                        </div>
-                        <div>
-                            Tags
-                        </div>
-                        <div className="row justify-content-center">
-                            Content
-                        </div>
+                <div className="card mb-4 m-5">
+                    {/* Post Details */}
+                    <div className="card-header">
+                        <small className="text-muted">Author: John Doe | Posted on: October 5, 2023</small>
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">Post Title</h5>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores saepe adipisci totam cum eum tempore veritatis odio, nulla est vero autem iusto ducimus, labore illo neque! Voluptatum non voluptates optio enim aliquid obcaecati aut similique. Voluptates consequuntur eius pariatur perspiciatis non dolores nihil quos, quod delectus ea laboriosam voluptatibus quasi. Necessitatibus illo, ipsam omnis placeat aperiam laboriosam autem rem, deleniti soluta tempore, accusantium tenetur?.</p>
+                    </div>
+                    <div className="p-4">
+                        <u>Comments: 21</u>
                     </div>
                 </div>
             </div>
