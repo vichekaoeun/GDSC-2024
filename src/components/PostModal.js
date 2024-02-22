30afea03e8636757ac7372e6277db2eac19cfe66
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-02-21 22:26:14 -0500

    add react-select

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './PostModal.css';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 
 export default function PostModal({ closeModal }) {
+    const animatedComponents = makeAnimated();
+
     return (
         <div className="modal-overlay" onClick={closeModal}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <div className="row">
-                    <h2 className="col">Create Post</h2>
+                    <h2 className="col">Create a Post</h2>
                     <div className="col d-flex justify-content-end">
                         <button className="btn btn-danger close-button rounded" onClick={closeModal}>
                             Close
@@ -15,22 +19,25 @@ export default function PostModal({ closeModal }) {
                 </div>
                 <form>
                     <div className="form-group">
-                        <label for="exampleFormControlInput1">Title</label>
-                        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Post Title"></input>
+                        <label for="blog-title">Title</label>
+                        <input type="text" className="form-control" id="blog-title" placeholder="Post Title"></input>
                     </div>
-                    <div className="form-group">
-                        <label for="exampleFormControlSelect1">Select a Tag</label>
-                        <select className="form-control" id="exampleFormControlSelect1">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
+                    <div className="form-group mt-3 mb-3">
+                        <Select
+                            closeMenuOnSelect={false}
+                            components={animatedComponents}
+                            isMulti
+                            options={[
+                                { value: 'chocolate', label: 'Chocolate' },
+                                { value: 'strawberry', label: 'Strawberry' },
+                                { value: 'vanilla', label: 'Vanilla' }
+                            ]}
+                        />
                     </div>
+
                     <div className="form-group">
                         <label for="exampleFormControlTextarea1">Content</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Write something here..."></textarea>
                     </div>
                     <button className="btn btn-primary mt-5">Submit</button>
                 </form>
