52683f06f540d87daf8abb7a46c45654b40d4ad7
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-06-06 19:32:18 -0400

    center text relative to video

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 
 export default function Content() {
     return (
-        <div className="d-flex flex-column align-items-center">
+        <div className="d-flex flex-column align-items-center position-relative">
             <video src={videoBg} autoPlay loop muted className="video-container" />
             <div className="overlay">
                 <h1 className="roboto-regular text-white underline-animation fade-in-container">Find a Community like you</h1>
