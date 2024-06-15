d4eec3fcbd1c96c63fac979875b4dcd5cb6d0d09
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-06-14 22:18:24 -0400

    add sidebar for smaller devices

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,8 @@ import '../scss/style.scss';
 import { Link } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import './nav.css';
+import hamburger from '../assets/hamburger.png';
+import cross from '../assets/cross.png';
 
 export default function Nav() {
     const auth = getAuth();
@@ -40,49 +42,77 @@ export default function Nav() {
         };
     }, []);
 
+    const showSidebar = () => {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.display = 'flex';
+    }
+
+    const hideSidebar = () => {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.display = 'none';
+    }
+
     return (
-        <nav className={`navbar navbar-expand-xxl sticky-top ${showNav ? 'show' : 'hide'}`} style={{ background: '#01CDA9', transition: 'top 0.3s' }}>
-            <div className="container-fluid">
+        <>
+            <nav className={`navbar navbar-expand-xxl sticky-top ${showNav ? 'show' : 'hide'}`} style={{ background: '#01CDA9', transition: 'top 0.3s' }}>
+                <div className="container-fluid">
 
 
-                <div className="row">
-                    <Link className="navbar-brand col-md-auto" to="/" style={{ fontFamily: "Times New Roman", fontSize: "50px", fontWeight: "bold", textShadow: "1px 1px 2px rgba(0, 0, 0, 0.1)" }}>Therapy Talks</Link>
+                    <div className="row">
+                        <Link className="navbar-brand col-md-auto" to="/" style={{ fontFamily: "Times New Roman", fontSize: "50px", fontWeight: "bold", textShadow: "1px 1px 2px rgba(0, 0, 0, 0.1)" }}>Therapy Talks</Link>
 
+                    </div>
+
+
+                    <div className="row" style={{ fontFamily: "Arial", fontSize: "20px" }}>
+                        <div className="navbar-item my-auto col-md-auto hideOnMobile">
+                            <Link className="nav-link active" to="/blog">Stories</Link>
+                        </div>
+
+                        <div className="navbar-item my-auto col-md-auto hideOnMobile">
+                            <Link className="nav-link active" to="/mission">Our Mission</Link>
+                        </div>
+
+                        <div className="navbar-item my-auto col-md-auto hideOnMobile">
+                            <a className="nav-link active" href="#">Programs</a>
+                        </div>
+
+                        <div className="navbar-item my-auto col-md-auto hideOnMobile">
+                            <a className="nav-link active" href="#">Communities</a>
+                        </div>
+
+                        <div className="navbar-item my-auto col-md-auto hideOnMobile">
+                            {isLoggedIn ? (
+                                <button className="btn btn-danger btn-lg login-button"><Link style={{ textDecoration: "none", color: "white" }} to="/profile" >Profile</Link></button>
+                            ) : (
+                                <button className="btn btn-danger btn-lg login-button"><Link style={{ textDecoration: "none", color: "white" }} to="/signin" >Login</Link></button>
+                            )}
+                        </div>
+                        <div className="navbar-item my-auto col-md-auto menu-button">
+                            <button className="button-reset" onClick={showSidebar}><img src={hamburger} alt="hamburger-icon" style={{ width: '50px', height: '40px' }} /></button>
+                        </div>
+                    </div>
                 </div>
+            </nav>
+            <div className="sidebar" style={{ fontFamily: "Arial", fontSize: "20px" }}>
+                <button className="button-reset" onClick={hideSidebar}><img src={cross} alt="cross-icon" style={{ width: '36px', height: '36px' }} /></button>
 
+                <Link className="nav-link active" to="/blog">Stories</Link>
 
-                <div className="row" style={{ fontFamily: "Arial", fontSize: "20px" }}>
-                    <div className="navbar-item my-auto col-md-auto">
-                        <Link className="nav-link active" to="/blog">Stories</Link>
-                    </div>
+                <Link className="nav-link active" to="/mission">Our Mission</Link>
 
-                    <div className="navbar-item my-auto col-md-auto">
-                        <Link className="nav-link active" to="/mission">Our Mission</Link>
-                    </div>
+                <a className="nav-link active" href="#">Programs</a>
 
-                    <div className="navbar-item my-auto col-md-auto">
-                        <a className="nav-link active" href="#">Programs</a>
-                    </div>
+                <a className="nav-link active" href="#">Communities</a>
 
-                    <div className="navbar-item my-auto col-md-auto">
-                        <a className="nav-link active" href="#">Communities</a>
-                    </div>
-
-                    <div className="navbar-item my-auto col-md-auto">
-                        {isLoggedIn ? (
-                            <button className="btn btn-danger btn-lg login-button"><Link style={{ textDecoration: "none", color: "white" }} to="/profile" >Profile</Link></button>
-                        ) : (
-                            <button className="btn btn-danger btn-lg login-button"><Link style={{ textDecoration: "none", color: "white" }} to="/signin" >Login</Link></button>
-                        )}
-                    </div>
-
-
-                </div>
+                {isLoggedIn ? (
+                    <button className="btn btn-danger btn-lg login-button"><Link style={{ textDecoration: "none", color: "white" }} to="/profile" >Profile</Link></button>
+                ) : (
+                    <button className="btn btn-danger btn-lg login-button"><Link style={{ textDecoration: "none", color: "white" }} to="/signin" >Login</Link></button>
+                )}
             </div>
 
-        </nav>
-
-
+        </>
     );
 }
 
