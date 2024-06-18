e5187490c18e3c3197c2e570e58dfeaa608763bd
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-06-17 21:37:48 -0400

    adjust front page sizing

--- src/components/graph1.js
+++ src/components/graph1.js
@@ -1,39 +1,36 @@
 import React, { useState } from 'react';
-import '../scss/style.scss';
-import '../scss/_variables.scss';
-import './Animation.css';
+import '../scss/style.scss'; // Ensure your SCSS file is imported
 import { PieChart, Pie, Cell } from 'recharts';
 import VisibilitySensor from 'react-visibility-sensor';
 import Grow from '@mui/material/Grow';
 
+const data = [
+    { name: 'Group Affected', value: 25 },
+    { name: 'Group Not Affected', value: 75 },
+];
+
+const COLORS = ['#01CDA9', '#B9F1E7'];
+const RADIAN = Math.PI / 180;
+
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
+
 export default function Graph1() {
     const [isVisible, setIsVisible] = useState(false);
-    const [activeIndex, setActiveIndex] = useState(-1);
 
     const onVisibilityChange = (isVisible) => {
         if (isVisible) {
             setIsVisible(true);
         }
-    };
-
-    const data = [
-        { name: 'Group Affected', value: 25 },
-        { name: 'Group Not Affected', value: 75 },
-    ];
-
-    const COLORS = ['#01CDA9', '#B9F1E7'];
-    const RADIAN = Math.PI / 180;
-
-    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN);
-        const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-        return (
-            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-                {`${(percent * 100).toFixed(0)}%`}
-            </text>
-        );
     };
 
     return (
@@ -43,12 +40,15 @@ export default function Graph1() {
                     <div className='col d-flex justify-content-center align-items-center mx-0'>
                         <div className='text-center custom-font rounded bg-white p-3 shadow'>
                             <h1 className='display-1 custom-red-font'>1 in 4</h1>
-                            <h3 className=''>"People will suffer from mental<br /> disorder, according to the<br /> <u className='custom-blue-font'>World Health Organization (WHO)</u>"</h3>
+                            <h3 className=''>
+                                "People will suffer from mental<br /> disorder, according to the<br />
+                                <u className='custom-blue-font'>World Health Organization (WHO)</u>"
+                            </h3>
                         </div>
                     </div>
                 </Grow>
             </VisibilitySensor>
-            <div className='col d-flex justify-content-center mx-0'>
+            <div className='col d-flex justify-content-center mx-0 pie-chart-container'>
                 <VisibilitySensor onChange={onVisibilityChange}>
                     <PieChart width={350} height={350}>
                         <Pie
